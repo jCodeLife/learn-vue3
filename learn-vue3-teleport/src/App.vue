8d86cc5c3b4853a32ef769c071eadaceafa4d312
lbj <bingjin.liao@outlook.com>
2020-11-11 23:32:40 +0800

    vue3 teleport的基本使用

---
+++ learn-vue3-teleport/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
+  <Modals><Modals/>
+</template>
+
+<script>
+import Modals from "./components/Modals.vue";
+
+export default {
+  name: "App",
+  components: {
+    Modals
+  },
+};
+</script>
---
+++ learn-vue3-teleport/src/components/Modals.vue
@@ -0,0 +1,52 @@
+<template>
+  <button @click="modalsOpen = true">弹出框按钮</button>
+  <teleport to='body'>
+    <div v-if="modalsOpen" class="modals">
+      <div>
+          <p>这是一个弹出框，并且是被传送到body上</p>
+          <button @click="modalsOpen = false">关闭</button>
+      </div>      
+    </div>
+  </teleport>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "Modals",
+  setup() {
+    let modalsOpen = ref(false);
+    return { modalsOpen };
+  },
+};
+</script>
+
+<style scoped>
+.modals{
+    background-color: rgba(0, 0, 0, .5);
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+.modals div{
+    border-radius: 4px;
+    width: 300px;
+    height: 100px;
+    background-color:#fff;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    font-size: .9em;
+    color:  rgba(0, 0, 0, .9);
+}
+button{
+    padding: 5px;
+}
+</style>
